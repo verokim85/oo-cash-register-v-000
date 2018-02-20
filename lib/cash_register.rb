14aaafd99d82b888d1e17c5788d30143137f70eb
Veronica Kim <vero.a.kim@gmail.com>
2018-02-20 02:03:52 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
 
+attr_accessor :total, :discount
+
+def initialize(discount= 0)
+  @total = 0
+  @discount = discount
+end
+
+def add_item(item, price, amount= 1)
+  self.total = total + price * amount
+end
+
+
+
+end
